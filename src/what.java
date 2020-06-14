a3d39f02140c9487b3bc6c7b99a8d69ca656adad
danny <rlaehdgus900@naver.com>
2020-06-14 19:56:29 +0900

    implemented create table

--- src/what.java
+++ src/what.java
@@ -17,7 +17,6 @@ import java.util.*;
 import java.sql.*;
 import java.io.*;
 
-
 public class what {
 
 	/************************************************************************************************************************
@@ -48,7 +47,7 @@ public class what {
 			FileWriter writer = null;
 
 			writer = new FileWriter(file, true);
-			
+
 			int k = 0;
 
 			if (parser == "TELNO") {
@@ -56,16 +55,16 @@ public class what {
 
 					k = result.indexOf("\"", indexing[j]) + 2;
 					writer.write(result.substring(k, result.indexOf(",", k)));
-					//System.out.println(result.substring(k, result.indexOf(",", k)));
+					// System.out.println(result.substring(k, result.indexOf(",", k)));
 					writer.write("\n");
 					if (j + 1 == how_many - 1) {
 						writer.write(result.substring(k, result.indexOf(",", k)));
 						writer.write("\n");
 					}
-					System.out.println("나 여기있어1");
-					//System.out.println(j);
+					// System.out.println("나 여기있어1");
+					// System.out.println(j);
 				}
-			}else {
+			} else {
 				for (int j = 0; j < how_many - 1; j++) {
 					k = result.indexOf("\"", indexing[j]) + 3;
 					writer.write(result.substring(k, result.indexOf("\"", k)));
@@ -75,12 +74,12 @@ public class what {
 						writer.write(result.substring(k, result.indexOf("\"", k)));
 						writer.write("\n");
 					}
-					System.out.println("나 여기있어2");
+					// System.out.println("나 여기있어2");
 					// System.out.println(j);
 				}
 			}
 			writer.close();
-	}
+		}
 
 	}
 
@@ -111,11 +110,9 @@ public class what {
 		}
 
 		rd.close();
-		
 
-		//String all_results = sb.toString();
-		//System.out.println(all_results);
-		String all_results = "\"SIGUN_NM\":\"포천시\",\"SIGUN_CD\":\"41650\",\"FACLT_DIV_NM\":\"전통시장\",\"MARKET_NM\":\"일동5일장\",\"TELNO\":null,\"REFINE_LOTNO_ADDR\":\"경기도 포천시 일동면 기산리 94-36 주차장 일원\",\"REFINE_ROADNM_ADDR\":\"경기도 포천시 일동면 기산리 94-36 주차장 일원\",\"REFINE_ZIP_CD\":\"11117\",\"REFINE_WGS84_LOGT\":\"127.3201243147\",\"REFINE_WGS84_LAT\":\"37.9593507063\"},{\"SIGUN_NM\":\"포천시\",\"SIGUN_CD\":\"41650\",\"FACLT_DIV_NM\":\"전통시장\",\"MARKET_NM\":\"이동5일장\",\"TELNO\":456456456,\"REFINE_LOTNO_ADDR\":\"경기도 포천시 이동면 장암리 509-2, 장암4리 마을회관 일원\",\"REFINE_ROADNM_ADDR\":\"경기도 포천시 이동면 장암리 509-2, 장암4리 마을회관 일원\",\"REFINE_ZIP_CD\":\"11111\",\"REFINE_WGS84_LOGT\":\"127.3671989743\",\"REFINE_WGS84_LAT\":\"38.0354045010\"},{\"SIGUN_NM\":\"포천시\",\"SIGUN_CD\":\"41650\",\"FACLT_DIV_NM\":\"전통시장\",\"MARKET_NM\":\"양문5일장\",\"TELNO\":null,\"REFINE_LOTNO_ADDR\":\"경기도 포천시 영중면 양문리 810-1 일원\",\"REFINE_ROADNM_ADDR\":\"경기도 포천시 영중면 양문리 810-1 일원\",\"REFINE_ZIP_CD\":\"11129\",\"REFINE_WGS84_LOGT\":\"127.2431030999\",\"REFINE_WGS84_LAT\":\"38.0019033063\"},{\"SIGUN_NM\":\"포천시\",\"SIGUN_CD\":\"41650\",\"FACLT_DIV_NM\":\"전통시장\",\"MARKET_NM\":\"송우5일장\",\"TELNO\":456456771123,\"REFINE_LOTNO_ADDR\":\"경기도 포천시 송우리 138-2 일원\",\"REFINE_ROADNM_ADDR\":\"경기도 포천시 송우리 138-2 일원\",\"REFINE_ZIP_CD\":\"11179\",\"REFINE_WGS84_LOGT\":\"127.1459997600\",\"REFINE_WGS84_LAT\":\"37.8273084147\"},{\"SIGUN_NM\":\"포천시\",\"SIGUN_CD\":\"41650\",\"FACLT_DIV_NM\":\"전통시장\",\"MARKET_NM\":\"내리5일장\",\"TELNO\":6458302351684,\"REFINE_LOTNO_ADDR\":\"경기도 포천시 내촌면 내리 389-9 일원\",\"REFINE_ROADNM_ADDR\":\"경기도 포천시 내촌면 내리 389-9 일원\",\"REFINE_ZIP_CD\":\"11189\",\"REFINE_WGS84_LOGT\":\"127.2269151872\",\"REFINE_WGS84_LAT\":\"37.7850091095\"}]}]}";
+		String all_results = sb.toString();
+		// System.out.println(all_results);
 		int[] name_index = new int[300];
 		int[] addr_index = new int[300];
 		int[] long_index = new int[300];
@@ -136,51 +133,58 @@ public class what {
 		// System.out.println(lati_index);
 		// System.out.println(pnum_index);
 
+		Scanner scan = new Scanner(System.in);
+		System.out.println("SQL Programming Test");
+
+		System.out.println("Connecting PostgreSQL database");
+		Connection conn1 = null;
+		Statement st1 = null;
+		ResultSet rs1 = null;
+
+		String url1 = "jdbc:postgresql://localhost:5432/final_project";
+		String user = "postgres";
+		String password = "1234";
+
+		try {
+			conn1 = DriverManager.getConnection(url1, user, password);
+			st1 = conn1.createStatement();
+			rs1 = st1.executeQuery("SELECT VERSION()");
+
+			if (rs1.next())
+				System.out.println(rs1.getString(1));
+		} catch (SQLException sqlEX) {
+			System.out.println(sqlEX);
+			// TODO Auto-generated catch block
+		}
+
+		System.out.println("Creating food, brand, stock, market  relations");
+
+		String query1 = "create table food(fName varchar(20),categoryID int, primary key(fName))";
+		String query2 = "create table brand(bName varchar(20), primary key(bName))";
+		String query4 = "create table Market(mID int, mName varchar(20), Location varchar(40), Longitude float, Latitude float, phoneNumber varchar(20), primary key(mID))";
+		String query3 = "create table Stock(quantitiy int, fName_Food varchar(20), bName_Brand varchar(20), mID_Market int, primary key(fName_Food, bName_Brand, mID_Market), "
+				+ "foreign key(fName_Food) references food(fName), foreign key(bName_Brand) references Brand(bName), foreign key(mID_Market) references Market(mID))";
 		
-		 Scanner scan = new Scanner(System.in);
-	      System.out.println("SQL Programming Test");
-	      
-	      System.out.println("Connecting PostgreSQL database");
-	      Connection conn1 = null;
-	      Statement st = null;
-	      ResultSet rs = null;
-	      
-	      String url1 = "jdbc:postgresql://localhost:5432/mydb";
-	      String user = "danny";
-	      String password = "1234";
-	      
-	      try {
-	         conn1 = DriverManager.getConnection(url1, user, password);
-	         st = conn1.createStatement();
-	         rs = st.executeQuery("SELECT VERSION()");
-	         
-	         if(rs.next())
-	            System.out.println(rs.getString(1));
-	      }catch(SQLException sqlEX) {
-	         System.out.println(sqlEX);
-	      }
+
+		try {
+			st1.executeUpdate(query1);
+			st1.executeUpdate(query2);
+			st1.executeUpdate(query4);
+			st1.executeUpdate(query3);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
-		
-		
-		 System.out.println("Creating food, brand, stock, market  relations");
-	      try {
-	         String query1 = "create table food(fName varchar(20),categoryID int)";
-	         String query2 = "create table brand(bName varchar(20), primary key(bName))";
-	         String query3 = "create table Stock(quantitiy int, fName_Food varchar(20), bName_Brand varchar(20), mID_Market varchar(20), primary key(fName_Food, bName_Brand, mID_Market), "
-	         		+ "foreign key(fName_Food) references food(fName), foreign key(bName_Brand) references Brand(bName), foreign key(mID_Market) references Market(mID))";
-	         String query4 = "create table Market(mID int, mName varchar(20), Location varchar(40), Longitude float, Latitude float, phoneNumber varchar(20))";
-	         conn1.executeUpdate(query1);
-	         conn1.executeUpdate(query2);
-	         conn1.executeUpdate(query3);
-	         conn1.executeUpdate(query4);
-	         System.out.println("finished");
-	      }catch(SQLException sqlEX) {
-	         System.out.println(sqlEX);
-	      }
-		
-		
-	      conn1.disconnect();
+		try {
+			rs1.close();
+			st1.close();
+			conn1.close();
+		} catch (SQLException sqlEX) {
+			System.out.println(sqlEX);
+		}
+
 	}
 
 }
