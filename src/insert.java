99b1961aecba3197c1bae1a0e67004ecbd8509b1
KIMSEOYEON97 <62339218+KIMSEOYEON97@users.noreply.github.com>
2020-06-15 18:28:34 +0900

    not bring together

---
+++ src/insert.java
@@ -0,0 +1,87 @@
+
+
+//과일 ID :1
+st.execute( "insert into Food values ( '사과',1) ");
+st.execute( "insert into Food values ( '배',1) ");
+st.execute( "insert into Food values ( '귤',1) ");
+st.execute( "insert into Food values ( '키위',1) ");
+st.execute( "insert into Food values ( '토마토',1) ");
+st.execute( "insert into Food values ( '수박',1) ");
+st.execute( "insert into Food values ( '딸기',1) ");
+st.execute( "insert into Food values ( '바나나',1) ");
+st.execute( "insert into Food values ( '망고',1) ");
+st.execute( "insert into Food values ( '체리',1) ");
+
+//유제품 ID:2
+st.execute( "insert into Food values ( '흰우유',2) ");
+st.execute( "insert into Food values ( '저지방 우유',2) ");
+st.execute( "insert into Food values ( '버터',2) ");
+st.execute( "insert into Food values ( '마가린',2) ");
+st.execute( "insert into Food values ( '연유',2) ");
+st.execute( "insert into Food values ( '치즈 큐브',2) ");
+st.execute( "insert into Food values ( '크림 치즈',2) ");
+st.execute( "insert into Food values ( '휘핑 크림',2) ");
+st.execute( "insert into Food values ( '요구르트',2) ");
+st.execute( "insert into Food values ( '요플레',2) ");
+
+
+//과자,초콜릿 ID : 3
+st.execute( "insert into Food values ( '쌀과자',3) ");
+st.execute( "insert into Food values ( '다이제',3) ");
+st.execute( "insert into Food values ( '오징어 땅콩',3) ");
+st.execute( "insert into Food values ( '포카칩',3) ");
+st.execute( "insert into Food values ( '빼빼로',3) ");
+st.execute( "insert into Food values ( '눈을 감자',3) ");
+st.execute( "insert into Food values ( '맛동산',3) ");
+st.execute( "insert into Food values ( '몰티져스 밀크 초콜렛',3) ");
+st.execute( "insert into Food values ( '트뤼플 초콜렛',3) ");
+st.execute( "insert into Food values ( '초코바',3) ");
+
+//소스 ,장 ID : 4
+st.execute( "insert into Food values ( '케찹',4) ");
+st.execute( "insert into Food values ( '마요네즈',4) ");
+st.execute( "insert into Food values ( '고추장',4) ");
+st.execute( "insert into Food values ( '진간장',4) ");
+st.execute( "insert into Food values ( '머스타드',4) ");
+st.execute( "insert into Food values ( '미림',4) ");
+st.execute( "insert into Food values ( '식초',4) ");
+st.execute( "insert into Food values ( '오리엔탈 드레싱',4) ");
+st.execute( "insert into Food values ( '코울슬로 드레싱',4) ");
+st.execute( "insert into Food values ( '파인애플 소스',4) ");
+
+//라면 ID : 5
+st.execute( "insert into Food values ( '비빔면',5) ");
+st.execute( "insert into Food values ( '신라면',5) ");
+st.execute( "insert into Food values ( '진라면',5) ");
+st.execute( "insert into Food values ( '짜파게티',5) ");
+st.execute( "insert into Food values ( '까르보 불닭 볶음면',5)");
+st.execute( "insert into Food values ( '너구리',5)");
+st.execute( "insert into Food values ( '열라면',5)");
+st.execute( "insert into Food values ( '투움바 스파게티 라면',5) ");
+st.execute( "insert into Food values ( '나가사끼 짬뽕 라면 ',5)");
+st.execute( "insert into Food values ( '오모가리 김치찌개 라면',5) ");
+
+
+//Brand insert문
+
+st.execute( "insert into Brand values( '농심')");
+st.execute( "insert into Brand values( 'CJ제일제당')");
+st.execute( "insert into Brand values( '오뚜기')");
+st.execute( "insert into Brand values( '롯데푸드')");
+st.execute( "insert into Brand values( '삼양')");
+st.execute( "insert into Brand values( '농협')");
+st.execute( "insert into Brand values( '매일유업')");
+st.execute( "insert into Brand values( '하림')");
+st.execute( "insert into Brand values( '빙그레')");
+st.execute( "insert into Brand values( '파리크라상')");
+st.execute( "insert into Brand values( '대한제당')");
+st.execute( "insert into Brand values( 'SPC삼립')");
+st.execute( "insert into Brand values( '신한성식품')");
+st.execute( "insert into Brand values( '동서식품')");
+st.execute( "insert into Brand values( '남양 유업')");
+st.execute( "insert into Brand values( '이즈니')");
+st.execute( "insert into Brand values( '필라델피아')");
+st.execute( "insert into Brand values( 'Dole')");
+st.execute( "insert into Brand values( '썬키스트')");
+st.execute( "insert into Brand values( '제스프리')");
+
