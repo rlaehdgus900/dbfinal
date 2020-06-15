eda1ae5e28240dbca2158fd6b0b5e96b207d9131
ByunJaegyeong <worud1208@ajou.ac.kr>
2020-06-16 01:01:22 +0900

    jaegyeong commit

--- src/what.java
+++ src/what.java
@@ -83,61 +83,79 @@ public class what {
 			}
 			writer.close();
 		}
-
-		
 	}
+
+	/************************************************************************************************************************
+	* openAPI에서 315개의 tuple 가져오기
+	*************************************************************************************************************************/
+
+	public static class ApiExplorer{
+		public void apiexplorer(String SIGUN_CD) throws IOException {
+			StringBuilder urlBuilder = new StringBuilder("https://openapi.gg.go.kr/MrktStoreM");
+			urlBuilder.append("?" + URLEncoder.encode("ServiceKey", "UTF-8") + "=0535a6471c7b4d8a9920c4c5830a3c98");
+			urlBuilder.append("&" + URLEncoder.encode("Type", "UTF-8") + "=" + URLEncoder.encode("xml", "UTF-8"));
+			urlBuilder.append("&" + URLEncoder.encode("pIndex", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8"));
+			urlBuilder.append("&" + URLEncoder.encode("pSize", "UTF-8") + "=" + URLEncoder.encode("100", "UTF-8"));
+			urlBuilder.append("&" + URLEncoder.encode("SIGUN_CD", "UTF-8") + "=" + URLEncoder.encode(SIGUN_CD, "UTF-8"));
+
+			URL url = new URL(urlBuilder.toString());
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Content-type", "application/json");
+			System.out.println("Response code: " + conn.getResponseCode());
+			BufferedReader rd;
+			if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+				rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			} else {
+				rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+			}
+
+			StringBuilder sb = new StringBuilder();
+			String line;
+			while ((line = rd.readLine()) != null) {
+				sb.append(line);
+			}
+			rd.close();
+			conn.disconnect();
+			System.out.println(sb.toString());
+	
+			String all_results = sb.toString();
+			// System.out.println(all_results);
+			int[] name_index = new int[300];
+			int[] addr_index = new int[300];
+			int[] long_index = new int[300];
+			int[] lati_index = new int[300];
+			int[] pnum_index = new int[300];
+			
+			checking plz = new checking();
+	
+			plz.check(name_index, all_results, "MARKET_NM");
+			plz.check(addr_index, all_results, "REFINE_ROADNM_ADDR");
+			plz.check(long_index, all_results, "REFINE_WGS84_LOGT");
+			plz.check(lati_index, all_results, "REFINE_WGS84_LAT");
+			plz.check(pnum_index, all_results, "TELNO");
+			
+			// System.out.println(name_index);
+			// System.out.println(addr_index);
+			// System.out.println(long_index);
+			// System.out.println(lati_index);
+			// System.out.println(pnum_index);
+		}
+
+	}
+	
 	/************************************************************************************************************************
 	 * parsing - done 
 	 *************************************************************************************************************************/
 	public static void main(String[] args) throws IOException {
-		StringBuilder urlBuilder = new StringBuilder("https://openapi.gg.go.kr/MrktStoreM");
-		urlBuilder.append("?" + URLEncoder.encode("ServiceKey", "UTF-8") + "=4f5be36dcb204f35a0786e7e1ab277c7");
-		urlBuilder.append("&" + URLEncoder.encode("Type", "UTF-8") + "=" + URLEncoder.encode("xml", "UTF-8"));
-		urlBuilder.append("&" + URLEncoder.encode("pIndex", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8"));
-		urlBuilder.append("&" + URLEncoder.encode("pSize", "UTF-8") + "=" + URLEncoder.encode("50", "UTF-8"));
-		urlBuilder.append("&" + URLEncoder.encode("SIGUN_CD", "UTF-8") + "=" + URLEncoder.encode("41650", "UTF-8"));
-
-		URL url = new URL(urlBuilder.toString());
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setRequestMethod("GET");
-		conn.setRequestProperty("Content-type", "application/json");
-		System.out.println("Response code: " + conn.getResponseCode());
-		BufferedReader rd;
-		if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-			rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		} else {
-			rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
-		}
-		StringBuilder sb = new StringBuilder();
-
-		String line;
-		while ((line = rd.readLine()) != null) {
-			sb.append(line);
-		}
-
-		rd.close();
-
-		String all_results = sb.toString();
-		// System.out.println(all_results);
-		int[] name_index = new int[300];
-		int[] addr_index = new int[300];
-		int[] long_index = new int[300];
-		int[] lati_index = new int[300];
-		int[] pnum_index = new int[300];
 		
-		checking plz = new checking();
+        String[] SIGUN_CDS = new String[]{"41820", "41000", "41280", "41290", "41210", "41610", "41310", "41410", "41570", "41360", "41250", "41190"};
+		ApiExplorer ae = new ApiExplorer();
 
-		plz.check(name_index, all_results, "MARKET_NM");
-		plz.check(addr_index, all_results, "REFINE_ROADNM_ADDR");
-		plz.check(long_index, all_results, "REFINE_WGS84_LOGT");
-		plz.check(lati_index, all_results, "REFINE_WGS84_LAT");
-		plz.check(pnum_index, all_results, "TELNO");
-
-		// System.out.println(name_index);
-		// System.out.println(addr_index);
-		// System.out.println(long_index);
-		// System.out.println(lati_index);
-		// System.out.println(pnum_index);
+        for(int i = 0; i<12; i++){
+            ae.apiexplorer(SIGUN_CDS[i]);
+		 }     
+		 
 		/***************************************************************************************************************************************
 		 * connect to Postgresql*
 		***************************************************************************************************************************************/
